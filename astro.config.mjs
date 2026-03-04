4306af700b0a6a21919e21fcf78a7e3f950c9613
saurabh <saurabhsharma2u@gmail.com>
2026-03-04 03:19:42 -0800

    wragler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
     integrations: [react()],
+    adapter: cloudflare(),
 });
